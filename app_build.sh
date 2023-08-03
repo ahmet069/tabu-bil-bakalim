3ab647f4a2b60a6b34ba0c9bde8b3b5206cae339
Ahmet Muhammet Subaşı <ahmet.subasi@kodzillaistanbul.com>
2023-08-03 10:33:09 +0300

    android changes

--- app_build.sh
+++ app_build.sh
@@ -1,2 +1,2 @@
 flutter build appbundle  --build-name=1.6 --build-number=7
-flutter build appbundle --no-tree-shake-icons --build-name=3.2 --build-number=23
+flutter build appbundle --no-tree-shake-icons --build-name=3.3 --build-number=24
